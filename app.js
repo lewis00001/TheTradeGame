ec15b6de7e73e86092fa87fd9d7ff506e5b90828
Lewis Lockhart <lewis00001@gmail.com>
2022-06-25 21:08:03 -0600

    adding progress on trader stuffs

--- app.js
+++ app.js
@@ -49,6 +49,55 @@ router.get('/createItemsTable', (req, res) => {
     });
 });
 
+// Create trade history table
+router.get('/createTradeHistoryTable', (req, res) => {
+    let sql = 'CREATE TABLE trade_history(id int NOT NULL AUTO_INCREMENT, trader_name VARCHAR(128), ' +
+                'item VARCHAR(255), sell_price_per_item int,num_items_traded int,PRIMARY KEY(id));';
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Items table created...');
+    });
+});
+
+// Insert into trade history table
+router.get('/addToTradeHistory/:traderName&:item&:sellPrice&:numItemsTraded', (req, res) => {
+    let post = {trader_name:req.params.traderName, item:req.params.item, 
+                sell_price_per_item:req.params.sellPrice, num_items_traded:req.params.numItemsTraded};
+    let sql = 'INSERT INTO trade_history SET ?';
+    let query = db.query(sql, post, (err, result) => {
+        if(err) throw err;
+        console.log("Trade History Event Added - Affected Rows: " + result.affectedRows);
+        res.send(result);
+    });
+});
+
+// // Select all player items helper
+// const selectAllTradeHistory = () => {
+//     console.log(`select all trade history events from the trade_history table`);
+//     return new Promise((resolve, reject) => {
+//         db.query(`SELECT pi.id, i.item, pi.qty, i.description FROM player_items pi ` +
+//                 `LEFT JOIN items i ON pi.id = i.id GROUP BY pi.id;`
+//         , (error, elements) => {
+//             if (error) {
+//                 return reject(error);
+//             }
+//             return resolve(elements);
+//         });
+//     });
+//  };
+
+// // Select all player items
+// router.get('/getAllPlayerItems', async (req, res) => {
+//     try {
+//         const results = await selectAllTradeHistory();
+//         res.status(200).json({elements : results});
+//     } catch (error) {
+//         console.log(error);
+//         res.sendStatus(500);
+//     }
+// });
+
 // Populate data in items table
 router.get('/populateItemsTable', (req, res) => {
     let sql = "INSERT INTO items(id,item,description) VALUES" + 
@@ -122,6 +171,7 @@ router.get('/addPlayerItem/:item&:quantity', (req, res) => {
     });
 });
 
+// Select all player items helper
 const selectAllPlayerItems = () => {
     console.log(`select all items from player_items with item descriptions from items table.`);
     return new Promise((resolve, reject) => {
@@ -136,7 +186,7 @@ const selectAllPlayerItems = () => {
     });
  };
 
- // Select all player items
+// Select all player items
 router.get('/getAllPlayerItems', async (req, res) => {
     try {
         const results = await selectAllPlayerItems();
@@ -147,6 +197,7 @@ router.get('/getAllPlayerItems', async (req, res) => {
     }
 });
 
+// select all items helper
 const selectAllItems = (tableName) => {
     console.log(`selectAllItems for ${tableName}`);
     return new Promise((resolve, reject) => {
